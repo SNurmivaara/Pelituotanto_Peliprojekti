161a2edde923fbbf3a4a3a099f751edc5103fb41
Ignacy Jan Paderewski, Prime Minister of Poland <sami.nurmivaara@gmail.com>
2015-03-31 17:19:56 +0300

    Lisätty Level luokkaan render() ja getTile() metodit
    Lisätty Tile luokkaan Tile-tyyppinen muuttuja voidTile
    
    Lisätty Sprite luokkaan vaihtoehtoinen konstruktori ja color() metodi

--- Pelituotanto/src/wildwestshootout/graphics/Sprite.java
+++ Pelituotanto/src/wildwestshootout/graphics/Sprite.java
@@ -13,6 +13,7 @@ public class Sprite {
 
     //Sprite -lisääminen! Täyttö: (Koko (pikseleitä X * X), Y koordinaatti, X koordinaatti, Spritesheet joka kyseessä)
     public static Sprite sand = new Sprite(16, 0, 0, SpriteSheet.tiles);
+    public static Sprite voidSprite = new Sprite(16, 0);
 
     //Konstruktori
     public Sprite(int size, int x, int y, SpriteSheet sheet) {
@@ -22,6 +23,20 @@ public class Sprite {
         this.y = y * size;
         this.sheet = sheet;
         load();
+    }
+    
+    //Vaihtoehtoinen konstruktori
+    public Sprite(int size, int color) {
+        this.SIZE = size;
+        this.pixels = new int[this.SIZE * this.SIZE];
+        setColor(color);
+    }
+
+    //Värin asettaminen
+    private void setColor(int color) {
+        for (int i = 0; i < this.SIZE * this.SIZE; i++) {
+            this.pixels[i] = color;
+        }
     }
 
     //load() metodi joka lataa tietyt pikselit (tietyn Spriten määritellystä SpriteSheet:istä
